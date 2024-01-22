e53739ae0c735d3ef4cfe49af84b6d55ac38c659
Kondexor2000 <96817669+Kondexor2000@users.noreply.github.com>
2024-01-22 14:41:20 +0100

    Update tests.py

--- blogapp/tests.py
+++ blogapp/tests.py
@@ -6,20 +6,25 @@ from .models import Post
 from .forms import PostForm
 
 class PostTestCase(TestCase):
+
+    # signup processing component test
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpassword')
 
+    # post creating component test
     def test_post_creation(self):
         post = Post.objects.create(title='Test Post', content='This is a test post.', author=self.user)
         self.assertEqual(post.title, 'Test Post')
         self.assertEqual(post.content, 'This is a test post.')
         self.assertEqual(post.author, self.user)
 
+    # no error in post creating component
     def test_post_form_valid(self):
         form_data = {'title': 'Test Post', 'content': 'This is a test post.'}
         form = PostForm(data=form_data)
         self.assertTrue(form.is_valid())
 
+    # they are errors in post creating component
     def test_post_form_invalid(self):
         form_data = {'title': '', 'content': ''}
         form = PostForm(data=form_data)
